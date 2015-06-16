d0efd2e42c0b2ce9651d928e422b92bfb0c119cd
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-06-16 18:54:16 +0200

    minimal example of a resharper 9.1 extension

---
+++ RS9-Example/MyCompletion.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using JetBrains.Application;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems.Impl;
+using JetBrains.ReSharper.Feature.Services.CSharp.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Features.Intellisense.CodeCompletion.CSharp.Rules;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+
+namespace RS9_Example
+{
+    [Language(typeof (CSharpLanguage))]
+    internal class MyCompletion : CSharpItemsProviderBasic
+    {
+        protected override bool IsAvailable(CSharpCodeCompletionContext context)
+        {
+            return true;
+        }
+
+        protected override bool AddLookupItems(CSharpCodeCompletionContext context, GroupedItemsCollector collector)
+        {
+            var item = new TextLookupItem("hello");
+            collector.Add(item);
+            return base.AddLookupItems(context, collector);
+        }
+    }
+
+    [ShellComponent]
+    internal class MyComponent
+    {
+        public MyComponent()
+        {
+            // trying to figure out, when/if this is loaded
+            File.WriteAllText(@"C:\Users\seb\MyComponent_ctor.txt", "it works");
+        }
+    }
+}
---
+++ package-and-deploy.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: this picks the first .csproj or .nuspec file in the working directory and builds a .nupkg from it
+:: to package a specific project or file, add the .csproj or .nupkg file name after the pack option
+:: -NoPackageAnalysis suppresses warnings, caused by R# bundles not adhering to the NuGet structure conventions
+
+set TIME1=%time::=%
+set TIME2=%TIME1: =0%
+set TIME3=%TIME2:~0,-3%
+::echo %TIME2%
+
+set DATE1=%date:~-4,4%%date:~-10,2%%date:~-7,2%
+::echo %DATE1%
+
+set TIMESTAMP=%DATE1%-%TIME3%
+echo Building @ %TIMESTAMP%...
+
+set TARGET=C:\Users\seb\Desktop\KaVE-Debug-Feed\
+
+::C:\Users\Sven\Downloads\
+nuget.exe pack RS9.Example.1.0.0-alpha1.nuspec -Verbosity detailed -NoPackageAnalysis -Version 1.0.0-v%TIMESTAMP%
+
+
+echo Deploying into: %TARGET%
+move *.nupkg %TARGET%
+
+pause
